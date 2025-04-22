cf49325bd92d528a47bae927e141da02eb41591e
Gunipoomm <siripoom.saen@bumail.net>
2025-04-22 22:47:42 +0700

    Final

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,25 +1,29 @@
+using UnityEngine;
+
 public class PlayerMovement : MonoBehaviour
 {
-    public float Speed; //public variable use Pascal casing
-    float move;         //private variable
+    public float Speed;
+    float move;
 
     public float JumpForce;
     public bool IsJumping;
 
     Rigidbody2D rb2d;
 
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
-        move = Input.GetAxis("Horizontal"); //x - axis
-
-        //use rigidbody2d to move left and right (x-axis)
+        move = Input.GetAxis("Horizontal");
         rb2d.linearVelocity = new Vector2(move * Speed, rb2d.linearVelocity.y);
 
-        //jump
         if (Input.GetButtonDown("Jump") && !IsJumping)
         {
             rb2d.AddForce(new Vector2(rb2d.linearVelocity.x, JumpForce));
-            Debug.Log("Jump"); //for debugging purpose
+            Debug.Log("Jump");
         }
     }
 
